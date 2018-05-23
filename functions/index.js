26fa722a48dfcb4abbe644661d6cb1391331268a
kome4489 <kome4489@yahoo.co.jp>
2018-05-23 22:27:09 +0900

    second commit

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,5 @@
 const functions = require('firebase-functions');
+const firebase = require('firebase');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -6,3 +7,20 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyADIElM3BZDSzAVboB0m11YKqXpRL5O0E4",
+    authDomain: "weather-e382e.firebaseapp.com",
+    databaseURL: "https://weather-e382e.firebaseio.com",
+    projectId: "weather-e382e",
+    storageBucket: "weather-e382e.appspot.com",
+    messagingSenderId: "563353375511"
+};
+firebase.initializeApp(config);
+
+exports.readDatebaseTest = functions.https.onRequest((request, response) => {
+    var ref = firebase.database().ref("user");
+    ref.on("value", function(snapshot) {
+      response.send(JSON.stringify({ message: snapshot }));
+    });
+});
